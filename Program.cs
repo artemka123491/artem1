0cd46217c7fb249469b2160862b65dacb84252fd
artemka123491 <103022185+artemka123491@users.noreply.github.com>
2022-04-05 09:43:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет! Как дела?");
+        }
+    }
+}
